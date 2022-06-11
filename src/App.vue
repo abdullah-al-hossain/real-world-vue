c1bb0aa5365e996a18caa04119904849c7502e29
shaheen2013 <coredeveloper.2013@gmail.com>
2022-06-11 23:49:23 +0600

    Route to link not working for name fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <nav>
-    <router-link :to="{name: 'EventListView'}">Events</router-link> |
-    <router-link :to="{name: 'AboutView'}">About</router-link> |
-    <router-link :to="{name: 'TestView'}">Test</router-link>
+    <router-link :to="{ name: 'EventListView' }">Events</router-link> |
+    <router-link :to="{ name: 'AboutView' }">About</router-link> |
+    <router-link :to="{ name: 'TestView' }">Test</router-link>
   </nav>
   <router-view />
 </template>
