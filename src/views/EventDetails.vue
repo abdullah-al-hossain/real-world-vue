41b09ea466c8a285f9722392d2a0c801f75ccd46
shaheen2013 <coredeveloper.2013@gmail.com>
2022-06-12 00:07:16 +0600

    build files updated

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -2,6 +2,7 @@
   <div v-if="event">
     <h1>{{ event.title }}</h1>
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
   </div>
 </template>
 
